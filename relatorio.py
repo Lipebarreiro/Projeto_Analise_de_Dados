412d44549001d26b11a0a5b13fcc2be2af0cc9fe
Felipe Barreiro <114102258+Lipebarreiro@users.noreply.github.com>
2024-09-09 17:12:32 -0300

    Add files via upload

--- relatorio.py
+++ relatorio.py
@@ -1,1 +1,8 @@
 # A disparidade na quantidade de cursos oferecidos entre as regiões Norte e Sudeste do Brasil reflete as desigualdades socioeconômicas e históricas do país. A região Sudeste, sendo mais urbanizada e industrializada, concentra maior infraestrutura educacional e recursos financeiros, o que resulta em um número significativamente maior de cursos oferecidos. Em contrapartida, a região Norte enfrenta desafios como menor densidade populacional, infraestrutura limitada e dificuldades de acesso, o que aumenta a desigualdade entre as regiões, limitando as oportunidades de acesso ao ensino superior para os moradores da região Norte.
+
+
+# Links de pesquisas: 
+# 1 - 
+# 2 - 
+# 3 - 
+# 4 - https://blog.unopar.com.br/conheca-os-cursos-mais-concorridos-do-sisu/#:~:text=Medicina%2C%20Direito%2C%20Engenharia%20Civil%2C,fazer%20a%20diferen%C3%A7a%20na%20sociedade.
